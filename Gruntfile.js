a2f5842e5a04ee3399b175dff4a9716bf80922b1
saumya <saumya>
2013-08-05 13:33:16 +0530

    Added CSS minification settings

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt){
 	grunt.initConfig({
 		//load the project configuration
 		pkg: grunt.file.readJSON('package.json'),
-		
+		//JS task
 		uglify: {
 			options: {
 		        banner: '/*! Done by, <%= pkg.name %>. On:<%= grunt.template.today("yyyy-mm-dd") %> */\n',
@@ -24,12 +24,30 @@ module.exports = function(grunt){
 		    	dest: 'js/applicationEntry.min.js'
 		    	*/
 		    }
+		},
+
+		//CSS task
+		cssmin: {
+		  allFiles: {
+		    options: {
+		      banner: '/*! Done by, <%= pkg.name %>. On:<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+		    },
+		    files: {
+		      //one file
+			    	"css/min/application.min.css": ["css/application.css"],
+			    	//multiple files
+			    	"css/min/kolbomobile.min.css": ["css/index.css","css/themes/kolbo-1-theme/kolbo-1-theme.css","css/themes/default/jquery.mobile.structure-1.3.0.css","css/application.css"]
+		    }
+		  }
 		}
 	});
 
 	// Load the plugin that provides the "uglify" task.
   	grunt.loadNpmTasks('grunt-contrib-uglify');
+  	//Load the CSS minification task
+  	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 	//grunt.registerTask(taskName, [description, ] taskFunction)
-	grunt.registerTask('default',['uglify']);
+	//grunt.registerTask('default',['uglify']);
+	grunt.registerTask('default',['uglify','cssmin']);
 };
